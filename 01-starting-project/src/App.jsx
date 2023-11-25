b074cb038e5b6ea350957852010758ed76c65b09
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2023-11-25 13:47:32 +0900

    Added: logic for adding projects

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,9 @@
 import {useState} from 'react';
 import NewProject from './assets/Components/NewProject.jsx';
 import NoProject from './assets/Components/NoProject.jsx';
-import Sidebar from './components/Sidebar.jsx';
+import Sidebar from './assets/Components/Sidebar.jsx';
+
+
 function App(){
   const[projectState,setProjectState]=useState({
     selectedProjectID:undefined,
@@ -16,10 +18,19 @@ function handleStartAddProject(){
     };
   });
 }
+let content;
+if(projectState.selectedProjectID===null){
+  content=<NewProject/>
+}else if(projectState.selectedProjectID===undefined){
+  content=<NoProject noProject={handleStartAddProject}/>
+  }
 return(
   <main className="h-screen my-8 flex gap-8">
-    <projectsSidebar/>
-    <NoProjectSelected/>
+    
+    <Sidebar addProject={handleStartAddProject}/>
+    {content}
+    
+    
     
   </main>
 )
