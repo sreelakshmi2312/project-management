7445106a239fb7bd6af054c9b6f12f707629d517
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2023-11-25 16:30:03 +0900

    Added:function to add project details

--- 01-starting-project/src/assets/Components/NewProject.jsx
+++ 01-starting-project/src/assets/Components/NewProject.jsx
@@ -6,13 +6,19 @@ export default function NewProject(){
     const description=useRef();
     const dueDate=useRef();
 
+    function handleSave(){
+        const enteredTitle=title.current.value;
+        const enteredDescription=description.current.value;
+        const enteredDueDate=dueDate.current.value;
+    }
+
     return <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end"> 
             <li><button className="text-stone-800 hover:text-stone-950 flex mr-4">Cancel</button></li>
-            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 flex ml-4">Save</button></li>
+            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 flex ml-4" onClick={handleSave}>Save</button></li>
         </menu>
         <div>
-            <Input ref={title} label="title"/>
+            <Input ref={title} label="title" />
             <Input ref={description} label="description" textarea/>
             <Input ref={dueDate} label="due date"/>
         </div>
