38c25940661c411c2769d096345908b6d10502e5
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2023-11-25 14:17:10 +0900

    Added:properties into parameters

--- 01-starting-project/src/assets/Components/Button.jsx
+++ 01-starting-project/src/assets/Components/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button({children,...props}){
-    return <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 mb-4">
+    return <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 mb-4" {...props}>
          {children}
     </button>
 }
--- 01-starting-project/src/assets/Components/Input.jsx
+++ 01-starting-project/src/assets/Components/Input.jsx
@@ -3,7 +3,7 @@ export default function Input({label,textarea,...props}){
  return(
     <p className="flex flex-col gap-1 my-4">
         <label className=" text-sm font-bold uppercase text-stone-500">{label}</label>
-        {textarea?<textarea className="{classes}"{...props}/>:<input className="classes"{...props}/>}
+        {textarea?<textarea className={classes}{...props}/>:<input className={classes}{...props}/>}
     </p>
  )
 }
