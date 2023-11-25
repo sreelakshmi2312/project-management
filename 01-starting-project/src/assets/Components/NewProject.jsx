867675c21309989894889fb698f7857bfded98ec
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2023-11-25 13:47:03 +0900

    Added: useRef for input

--- 01-starting-project/src/assets/Components/NewProject.jsx
+++ 01-starting-project/src/assets/Components/NewProject.jsx
@@ -1,13 +1,20 @@
+import {useRef} from 'react';
+import Input from './Input';
+
 export default function NewProject(){
+    const title=useRef();
+    const description=useRef();
+    const dueDate=useRef();
+
     return <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end"> 
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
-            <Input label="Title"/>
-            <Input label="Description" textarea/>
-            <Input label="Due Date"/>
+            <Input ref={title}/>
+            <Input ref={description} textarea/>
+            <Input ref={dueDate} />
         </div>
     </div>
 }
